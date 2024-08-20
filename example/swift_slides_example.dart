12751449f5bb92b125185b060733f5edba40e2d7
Dsachith <dsachith3431@gmail.com>
2024-08-20 15:36:03 +0530

    Initial commit

---
+++ example/swift_slides_example.dart
@@ -0,0 +1,6 @@
+import 'package:swift_slides/swift_slides.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/swift_slides.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/swift_slides_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
